48074a933144b88bf59b32761c37b4d400d1018a
Leon Haider <leonhaider@icloud.com>
2024-02-16 16:33:07 +0100

    backend styling

--- PushTraffic-Theme/functions.php
+++ PushTraffic-Theme/functions.php
@@ -11,3 +11,7 @@ require_once(get_template_directory() . '/inc/media.php');
 require_once(get_template_directory() . '/inc/sidebars.php');
 
 require_once(get_template_directory() . '/inc/cpt.php');
+
+//Backend-Styling
+add_theme_support('editor-styles');
+add_editor_style('assets/css/style-editor.css');
